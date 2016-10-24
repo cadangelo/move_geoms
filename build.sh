f4ce8a236b994a13e10d10984cfe4e65798be2bf
chelsea <cadangelo@wisc.edu>
2016-10-24 12:37:55 -0500

    In process of adding ability to move individual vols, not working yet

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #/bin/bash
 
-MOAB_PATH=$HOME/research/moab/
-MOAB_LIBRARY=$MOAB_PATH"/lib"
-MOAB_INCLUDE=$MOAB_PATH"/include"
+MOAB_PATH=$HOME/opt_n/moab/
+MOAB_LIBRARY=$MOAB_PATH"lib"
+MOAB_INCLUDE=$MOAB_PATH"include"
+DAGMC_PATH=$HOME/opt_n/dagmc/
+DAGMC_LIBRARY=$DAGMC_PATH"lib"
 
 echo $MOAB_LIBRARY
+echo $DAGMC_LIBRARY
 
-g++ move_geoms.cpp -g -std=c++11 -I$MOAB_INCLUDE -L$MOAB_LIBRARY -lMOAB -o move
+g++ move_geoms.cpp -std=c++11 -g -I$MOAB_INCLUDE -L$MOAB_LIBRARY -ldagmc -lMOAB -o move
+
--- move_geoms.cpp
+++ move_geoms.cpp
@@ -3,8 +3,13 @@
 #include "moab/Interface.hpp"
 #include "moab/Types.hpp"
 #include "MBTagConventions.hpp"
+
+#include "DagMC.hpp"
+using moab::DagMC;
+
 #include <string>
 #include <math.h>
+#include <cmath>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -18,6 +23,11 @@ moab::Tag geom_tag;
 moab::Tag name_tag;
 moab::Tag obj_name_tag;
 moab::Tag dim_tag, id_tag;
+moab::Tag move_tag;
+moab::Tag obb_tag;
+moab::Tag obb_tree_tag;
+
+moab::DagMC *DAG;
 
 moab::ErrorCode get_all_handles(moab::Core *mbi)
 {
@@ -29,6 +39,10 @@ moab::ErrorCode get_all_handles(moab::Core *mbi)
 
   rval = mbi->tag_get_handle( "OBJECT_NAME", 32, moab::MB_TYPE_OPAQUE,
 			      obj_name_tag, moab::MB_TAG_SPARSE|moab::MB_TAG_CREAT);
+  CHECK_ERR(rval);
+
+  rval = mbi->tag_get_handle( "MOVE_TAG", 32, moab::MB_TYPE_OPAQUE,
+			      move_tag, moab::MB_TAG_SPARSE|moab::MB_TAG_CREAT);
   CHECK_ERR(rval);
 
   int negone = -1;
@@ -49,9 +63,45 @@ moab::ErrorCode get_all_handles(moab::Core *mbi)
 			      moab::MB_TAG_SPARSE|moab::MB_TAG_CREAT );
 
   CHECK_ERR(rval);
+/*
+  rval = mbi->tag_get_handle( MB_OBB_TREE_TAG_NAME, 1, MB_TYPE_HANDLE, 
+                                obb_tree_tag, MB_TAG_DENSE );
+  CHECK_ERR(rval);
+
+  rval = mbi->tag_get_handle( MB_OBB_TAG_NAME, 1, MB_TYPE_HANDLE,
+                                obb_tag, MB_TAG_DENSE );
+  CHECK_ERR(rval);
+
+*/
   return moab::MB_SUCCESS;
 }
-  
+/*
+void get_xyz_dists(double dx, double dy, double dz, double total_dist, int num_shots, double &dist_x, double &dist_y, double &dist_z)
+{
+  double mag;
+  //double unit_vec[3];
+  double dist_per_shot;
+
+  mag = sqrt(pow(dx, 2.0) + pow(dy, 2.0) + pow(dz, 2.0));
+  ux[0] = dx/mag;
+  uy[1] = dy[1]/mag;
+  uz[2] = dz[2]/mag;
+
+  dist_per_shot = total_dist/num_shots;
+  dist_x = dist_per_shot*ux;
+  dist_y = dist_per_shot*uy;
+  dist_z = dist_per_shot*uz;
+}
+
+double* get_dist_uvec_td(double total_dist, double dist_per_shot, double unit_vec[3])
+{
+  static double dist[3];
+
+
+  return dist;
+}
+
+ */ 
 int main(int argc, char* argv[]) {
 
   moab::Core *mbi = new moab::Core();
@@ -61,88 +111,111 @@ int main(int argc, char* argv[]) {
   get_all_handles(mbi);
 
   moab::ErrorCode rval;  
-  rval = mbi->load_file("big_graveyard.h5m");
-  
-  moab::Range::iterator it;
-  int id_num = 1;// ent_sets.size();
-  int dim;
-  moab::Range parents;
-  moab::Range ent_sets;
-  moab::Range vert_set;
-  const double x=0.0, y=0.0, z=0.0;
-  double xyz[3], xyz_trans[3];
+  rval = mbi->load_file("test_tag.h5m");
 
-  double total_dist = 5000.0;
-  double dist;
-  double dir_vec[3] = {1.0, 0.0, 0.0};
-  double mag;
-  double unit_vec[3];
-  double speed = 1.0;
-  double time; 
-  double dist_per_shot;
-  int num_shots = 2;
-  int shot_num = 0;
-  
+  DAG = new moab::DagMC(mbi);
 
-  time = total_dist/speed;
-  dist_per_shot = total_dist/num_shots;
-  mag = sqrt(pow(dir_vec[0], 2.0) + pow(dir_vec[1], 2.0) + pow(dir_vec[2], 2.0));
-  unit_vec[0] = dir_vec[0]/mag;
-  unit_vec[1] = dir_vec[1]/mag;
-  unit_vec[2] = dir_vec[2]/mag;
-  std::cout << "unit " << unit_vec[0] << std::endl;
-//  double trans_vec[3] = {1.0, 1.0, 1.0};
-
-  char buffer [33];
-
-
-  rval =  mbi->get_entities_by_type(0, moab::MBTRI, ent_sets);
+  rval = DAG->load_existing_contents();
   CHECK_ERR(rval);
-  std::cout << "size " << ent_sets.size() << std::endl;
+
+  rval = DAG->setup_obbs();
+  CHECK_ERR(rval);
  
-  rval = mbi->get_vertices(ent_sets, vert_set);
+  rval = DAG->setup_indices();
+  CHECK_ERR(rval);
+
+  // get moving tag from parse_properties
+  std::vector<std::string> group_name;
+  std::map<std::string, std::string> group_name_synonyms;
+
+  group_name.push_back("moving");
+
+  rval = DAG->parse_properties(group_name, group_name_synonyms);
+  if (moab::MB_SUCCESS != rval) 
+    {
+      std::cerr << "DAGMC failed to parse metadata properties" <<  std::endl;
+      exit(EXIT_FAILURE);
+    }
   
-  dist = dist_per_shot;
-  while (dist <= total_dist)
+  // get all volumes
+  int num_cells = DAG->num_entities( 3 );
+  std::cout << "num cells: " << num_cells << std::endl;
+ 
+  // get all triangles in vols that are moving
+  moab::EntityHandle moving;
+  moab::Range tris;
+  moab::Range vert_set;
+  moab::Range move_sets;
+  move_sets.clear();
+
+  rval = mbi->create_meshset(moab::MESHSET_SET, moving);
+  CHECK_ERR(rval);
+
+  for( int i = 1; i <= num_cells; ++i ) 
+    {  
+      moab::EntityHandle vol = DAG->entity_by_index( 3, i );
+      if( DAG->has_prop( vol, "moving"))
+        {
+          move_sets.insert(vol);
+
+          mbi->add_entities(moving, &vol, 1);
+        }
+    }
+  std::cout << "num move vols " << move_sets.size() << std::endl;
+
+  int num_tris;
+  rval =  mbi->get_number_entities_by_type(moving, moab::MBTRI, num_tris);
+  CHECK_ERR(rval);
+  std::cout<< "num dim 3" << num_tris << std::endl;
+
+  rval =  mbi->get_entities_by_type(moving, moab::MBTRI, tris);
+  CHECK_ERR(rval);
+  std::cout << "num tris " << tris.size() << std::endl;
+
+  rval = mbi->get_vertices(tris, vert_set);
+  CHECK_ERR(rval);
+  std::cout << "num moving verts: " << vert_set.size() << std::endl;
+
+  // move verts according to transformation
+  moab::Range::iterator it;
+  const double x=0.0, y=0.0, z=0.0;
+  double xyz[3], xyz_new[3];
+ 
+//  double unit_vec[3];
+  double dist[3];
+  double v[3] = {100, 0, 0};
+  double a[3] = {0, 0, 0};
+//  double speed = 1.0;
+  double t = 0.0; 
+  double ts = 1.0; 
+  double end_t = 20.0;
+  int shot_num = 0;
+//  time = total_dist/speed;
+//  dist_per_shot = total_dist/num_shots;
+//  double* unit_vec[3] = get_unit_vec(dir_vec);
+//  double* dist[3] = get_dist_uvec_td(total_dist, dist_per_shot, unit_vec);
+  
+//  rval = mbi->tag_delete(obb_tag);
+//  rval = mbi->tag_delete(obb_tree_tag);
+
+  while (t < end_t)
     {
       shot_num++;
+      t = t + ts;
       for (it = vert_set.begin(); it != vert_set.end(); ++it)
         {  
           rval = mbi->get_coords(&(*it), 1, xyz);
-          xyz_trans[0] = xyz[0] + dist_per_shot*unit_vec[0];
-          xyz_trans[1] = xyz[1] + dist_per_shot*unit_vec[1];
-          xyz_trans[2] = xyz[2] + dist_per_shot*unit_vec[2];
-          rval = mbi->set_coords(&(*it), 1, xyz_trans);
-         
+          xyz_new[0] = xyz[0] + v[0]*t + 0.5*a[0]*pow(t,2);
+          xyz_new[1] = xyz[1] + v[1]*t + 0.5*a[1]*pow(t,2);
+          xyz_new[2] = xyz[2] + v[2]*t + 0.5*a[2]*pow(t,2);
+          rval = mbi->set_coords(&(*it), 1, xyz_new);
+        
         }
-
-      dist = dist + dist_per_shot;
-//          rval = mbi->write_mesh((char)shot_num+output_file.c_str());
+      
       rval = mbi->write_mesh( (std::to_string(shot_num)+output_file).c_str());
 
     }
   
-/*  rval =  mbi->get_entities_by_type_and_tag(0, moab::MBENTITYSET, &geom_tag, NULL, 1, ent_sets);
-  CHECK_ERR(rval);
-  for (it = ent_sets.begin(); it != ent_sets.end(); ++it) {
-    rval = mbi->tag_get_data(geom_tag, &(*it), 1, &dim);
-    CHECK_ERR(rval);
-    if (dim == 2) {
-      rval = mbi->tag_set_data(id_tag, &(*it), 1, &(id_num));
-      CHECK_ERR(rval);
-      parents.clear();
-      rval = mbi->get_parent_meshsets(*it, parents);
-      CHECK_ERR(rval);
-      rval = mbi->tag_set_data(id_tag, &(*parents.begin()), 1, &(id_num));
-      CHECK_ERR(rval);
-      ++id_num;
-    }
-  }
-  
-  
-  // save file
-  rval = mbi->write_mesh(output_file.c_str());
- */ 
-
+  delete DAG;
   return 0;
 }    
