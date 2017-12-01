d5517813235c47287a1ba39fed7bccda43338062
chelsea <cadangelo@wisc.edu>
2017-11-30 19:39:17 -0600

    started capability to add multimple transitions

--- move_geoms.cpp
+++ move_geoms.cpp
@@ -230,9 +230,10 @@ moab::ErrorCode test_get_tagged_entities(moab::Core *mbi, int total_cells,
 }
 moab::ErrorCode new_get_tagged_entities(moab::Core *mbi, int total_cells, 
                                     std::string tag_name, 
-                                    moab::Range &tagged_vols,
-                                    moab::Range &tagged_surfs,
-                                    moab::Range &tagged_verts)//,
+                                    std::map<std::string, moab::Range> &tr_vols_map)
+//                                    moab::Range &tagged_vols,
+//                                    moab::Range &tagged_surfs,
+//                                    moab::Range &tagged_verts)//,
 //                                    moab::Range &tagged_vert_sets)
 {
   moab::ErrorCode rval;
@@ -260,62 +261,86 @@ moab::ErrorCode new_get_tagged_entities(moab::Core *mbi, int total_cells,
   int num_verts;
   moab::Range::iterator it, itr;
 
-  surf_set.clear();
-  vert_set.clear();
 
   rval = mbi->create_meshset(moab::MESHSET_SET, tagged_meshset);
   //CHECK_ERR(rval);
 
-  std::vector<std::string> properties;
+//  std::vector<std::string> properties;
   std::string val;
+//  std::map<std::string, moab::Range> tr_vols_map; 
 
   for( int i = 1; i <= total_cells; ++i ) 
     {  
       moab::EntityHandle vol = DAG->entity_by_index( 3, i );
       if( DAG->has_prop( vol, tag_name))
         {
- 
+
+          // get the value of vol's TR number (val) 
           rval = DAG->prop_value(vol, tag_name, val);
           std::cout << "val " << vol << " " << val << std::endl;
-//          std::cout << "properties[0] " << vol << " " << properties[0].c_str() << std::endl;
-//
-//          // loop over the properties and check for any mention
-//          // of the 2nd delimiter, if so extract from 0
-//          // to second delimiter
-//          // by being here we already know he property exists
-//          // being found upto the first delimiter
-//          if (tag_delims.size() > 1) {
-//          std::cout << "if tag delims " << std::endl;
-//            for (int j = 0 ; j < properties.size() ; j++) {
-//              size_t npos = 0, first = npos;
-//              npos = properties[j].find(tag_delims[1]);
-//              // extract from the match - which is either first
-//              // match or .length()
-//              properties[j] = properties[j].substr(0, npos);
-//              std::cout << "properties[j]" << j << " " << properties[j] << std::endl;
-//            }
-//          }
 
+          // get the vol's vertices
+//          rval = mbi->get_child_meshsets(vol, surf_set);
+//          CHECK_ERR(rval);
+//          for (it = surf_set.begin(); it != surf_set.end(); it++)
+//            {
+//              tagged_surfs.insert(*it);
+//              rval =  mbi->get_entities_by_type(*it, moab::MBVERTEX, vert_set);
+//              CHECK_ERR(rval);
+//              for (itr = vert_set.begin(); itr != vert_set.end(); itr++)
+//                {
+//                  tagged_verts.insert(*itr);
+//                }
 
-          tagged_vols.insert(vol);
+            std::cout << "before map" << std::endl; 
+            std::cout << "map size" << tr_vols_map.size() <<  std::endl; 
+          std::map<std::string, moab::Range>::iterator itt;
+          bool added = false;
+          std::string tr_num;
+          //if the map is not empty, look for a key that matches the tr # of the volume
+          if(tr_vols_map.size() > 0){
+            for(itt = tr_vols_map.begin(); itt != tr_vols_map.end(); ++itt){
+                
+               //get value of tr #
+               tr_num = itt->first;
+               std::cout << "tr num already in map" << tr_num << std::endl;
+               std::cout << "val num " << val << std::endl; 
+               //if tr# of current vol matches one already in map, add vol to range 
+               // added = true as soon as vol added to a range
+               if(val == tr_num){
+                 tr_vols_map[tr_num].insert(vol); 
+                 std::cout << "size of range for that tr #" << tr_vols_map[tr_num].size() << std::endl;
+                 std::cout << "first elemet" << *tr_vols_map[tr_num].begin() << std::endl;
+                 added = true;
+               }
+               // if added not true, keep looping, if true exit loop
+               if(added == true)
+                 break;
+            }
+          }
+          //if map empty or no matching key found, create key
+          //and add vol to range
+          //if(tr_vols_map.size() == 0 | added == false) {
+          if(added == false) {
+            tr_num = val;
+            tr_vols_map[tr_num].insert(vol); 
+            std::cout << "size of range for that tr #" << tr_vols_map[tr_num].size() << std::endl;
+            std::cout << "first elemet" << *tr_vols_map[tr_num].begin() << std::endl;
+            added = true;
+    
+          }
+          
+            std::cout << "after map" << std::endl; 
+          
+
+          //tagged_vols.insert(vol);
 
           //create map of tr#s : range of vols (or verts?)
           //return map from this fxn
 
          
 
-          rval = mbi->get_child_meshsets(vol, surf_set);
-          CHECK_ERR(rval);
-          for (it = surf_set.begin(); it != surf_set.end(); it++)
-            {
-              tagged_surfs.insert(*it);
-              rval =  mbi->get_entities_by_type(*it, moab::MBVERTEX, vert_set);
-              CHECK_ERR(rval);
-              for (itr = vert_set.begin(); itr != vert_set.end(); itr++)
-                {
-                  tagged_verts.insert(*itr);
-                }
-            }  
+          //  }  
         }
           
     }
@@ -681,6 +706,42 @@ moab::ErrorCode get_orig_vert_position(moab::Range mv , std::map<moab::EntityHan
       orig_positions[*itt] = p;
     }
 }
+moab::ErrorCode  get_moving_verts(std::map<std::string, moab::Range> tr_vols_map,
+                                  moab::Range &mv,
+                                  std::map<std::string, moab::Range> &tr_verts_map)
+{
+  moab::ErrorCode rval;
+  std::string tr_num;
+  moab::Range surf_set, vert_set;
+  moab::Range::iterator its, itv, itr; 
+  std::map<std::string, moab::Range>::iterator ittr;
+
+  for(ittr = tr_vols_map.begin(); ittr != tr_vols_map.end(); ++ittr){
+    tr_num = ittr->first;
+    std::cout << "TR # " << tr_num << std::endl;
+    for(itv = tr_vols_map[tr_num].begin(); itv != tr_vols_map[tr_num].end(); ++itv){
+      std::cout << "VOL # " << *itv << std::endl;
+      // get the vol's vertices
+      surf_set.clear();
+      rval = mbi->get_child_meshsets(*itv, surf_set);
+      CHECK_ERR(rval);
+      for (its = surf_set.begin(); its != surf_set.end(); its++){
+        vert_set.clear();
+        rval =  mbi->get_entities_by_type(*its, moab::MBVERTEX, vert_set);
+        CHECK_ERR(rval);
+        // insert verts into range of all moving verts
+        // and into the map of TR #'s to verts
+        for (itr = vert_set.begin(); itr != vert_set.end(); itr++){
+            mv.insert(*itr);
+            tr_verts_map[tr_num].insert(*itr);
+        }
+      }
+    }
+    std::cout << "num verts in this tr num " << tr_verts_map[tr_num].size() << std::endl;
+  }
+  std::cout << "num all moving verts" << mv.size() << std::endl;
+  return moab::MB_SUCCESS;
+}
 int main(int argc, char* argv[]) 
 {
   moab::ErrorCode rval; 
@@ -696,14 +757,24 @@ int main(int argc, char* argv[])
 
   //get moving volumes, surfs, and verts
   moab::Range vols, surfs, mv;
-  std::map<moab::EntityHandle, XYZ> orig_positions;
-  rval = new_get_tagged_entities(mbi, num_cells, "tr", vols, surfs, mv);
+  //rval = new_get_tagged_entities(mbi, num_cells, "tr", vols, surfs, mv);
+  std::map<std::string, moab::Range> tr_vols_map;
+  rval = new_get_tagged_entities(mbi, num_cells, "tr", tr_vols_map);
   //rval = test_get_tagged_entities(mbi, num_cells, "tr", vols, surfs, mv);
+
+  moab::Range surf_set, vert_set;
+  moab::Range::iterator it, itr;
+  surf_set.clear();
+  vert_set.clear();
+
   std::cout << "num moving vols " << vols.size() << std::endl;
-  std::cout << "num moving verts " << mv.size() << std::endl;
+  //std::cout << "num moving verts " << mv.size() << std::endl;
 
   // map of vertex eh to original position
+  std::map<moab::EntityHandle, XYZ> orig_positions;
   std::map<moab::EntityHandle, XYZ> position;
+  std::map<std::string, moab::Range> tr_verts_map;
+  rval = get_moving_verts(tr_vols_map, mv, tr_verts_map);
   rval = get_orig_vert_position(mv, position);
 
   //process input function
@@ -730,57 +801,66 @@ int main(int argc, char* argv[])
   double t = 0.0; //current time [s]
   int shot_num = 0; //current time step
 
+  std::string tr_num;
  
   //base output file name 
   std::string output_file = "moved.h5m";
 
-  moab::Range::iterator its, itt, itv, itx, itz;
+  moab::Range::iterator its, itt, itv, itx, itz, itvt;
+  std::map<std::string, moab::Range>::iterator ittr, itrv;
   std::cout << "t step " << ts << std::endl;
   std::cout << "end time " << end_t << std::endl;
+  
 
-  while (t <= end_t)
-    {
+  //set end_t to 1 for relocation translations (single time step)
+  // for velocity vectors that need broken into time steps,
+  // set start_t and end_t to full length of time
+  while (t <= end_t){
       std::cout << "time step t= " << t << std::endl;
-      for (its = vols.begin(); its != vols.end(); ++its)
-        {
-          for (itt = mv.begin(); itt != mv.end(); ++itt)
-            {
-                  //get original position
-                  p_0 = position[*itt];
-           
-                  //if translation
-                  if (translation == 1)
-                    {
-                       p_new.x = p_0.x + v_0.x*t + (1/2)*b.x*pow(t,2) + (1/6)*c.x*pow(t,3) + (1/12)*d.x*pow(t,4);
-                       p_new.y = p_0.y + v_0.y*t + (1/2)*b.y*pow(t,2) + (1/6)*c.y*pow(t,3) + (1/12)*d.y*pow(t,4);
-                       p_new.z = p_0.z + v_0.z*t + (1/2)*b.z*pow(t,2) + (1/6)*c.z*pow(t,3) + (1/12)*d.z*pow(t,4);
-                    }
-               
-                  //if rotation
-                  if (rotation == 1)
-                    { 
-                      double omega = omega_0 + alpha[0]*t + (1/2)*alpha[1]*pow(t,2) + (1/3)*alpha[2]*pow(t,3);
-                      theta = omega*t;
-                      p_new = rotate_point(p_0, theta, L0, L1);
-                    }
-         
-                  //set the coordinates of the updated position  
-                  xyz_new[0] = p_new.x;
-                  xyz_new[1] = p_new.y;
-                  xyz_new[2] = p_new.z;
-                  rval = mbi->set_coords(&(*itt), 1, xyz_new);
-                  CHECK_ERR(rval);
 
-            }    
+    //for each TR #
+    for(itrv = tr_verts_map.begin(); itrv != tr_verts_map.end(); ++itrv){
+       tr_num = itrv->first;
+       
+       //create map of TR #'s to motion vectors
+       // then can move each vert w/ correct TR
+
+      //for each vert
+      for(itvt =  tr_verts_map[tr_num].begin(); itvt !=  tr_verts_map[tr_num].end(); ++itvt){
+        //get original position
+        p_0 = position[*itvt];
+      
+        //if translation
+        if (translation == 1){
+             p_new.x = p_0.x + v_0.x*t + (1/2)*b.x*pow(t,2) + (1/6)*c.x*pow(t,3) + (1/12)*d.x*pow(t,4);
+             p_new.y = p_0.y + v_0.y*t + (1/2)*b.y*pow(t,2) + (1/6)*c.y*pow(t,3) + (1/12)*d.y*pow(t,4);
+             p_new.z = p_0.z + v_0.z*t + (1/2)*b.z*pow(t,2) + (1/6)*c.z*pow(t,3) + (1/12)*d.z*pow(t,4);
         }
         
-//      rval = mbi->write_mesh( (std::to_string(shot_num)+output_file).c_str());
-      shot_num++;
-      std::cout << "shot num " << shot_num << std::endl;
-      t = t + ts;
+        //if rotation
+        if (rotation == 1){ 
+            double omega = omega_0 + alpha[0]*t + (1/2)*alpha[1]*pow(t,2) + (1/3)*alpha[2]*pow(t,3);
+            theta = omega*t;
+            p_new = rotate_point(p_0, theta, L0, L1);
+        }
       
-     }
-  
+        //set the coordinates of the updated position  
+        xyz_new[0] = p_new.x;
+        xyz_new[1] = p_new.y;
+        xyz_new[2] = p_new.z;
+        rval = mbi->set_coords(&(*itvt), 1, xyz_new);
+        CHECK_ERR(rval);
+      }//move each vertex   
+ 
+    }//for each TR #
+      
+//    rval = mbi->write_mesh( (std::to_string(shot_num)+output_file).c_str());
+    shot_num++;
+    std::cout << "shot num " << shot_num << std::endl;
+    t = t + ts;
+      
+  }//while
+
   delete DAG;
   return 0;
 }    
